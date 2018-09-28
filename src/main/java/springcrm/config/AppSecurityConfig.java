72cc77fdd5a0a40d27fd66ce6c3cdd67af6b3f9c
andersengenolsen <anderseols@gmail.com>
2018-09-28 12:39:03 +0200

    Fixed security hole

--- src/main/java/springcrm/config/AppSecurityConfig.java
+++ src/main/java/springcrm/config/AppSecurityConfig.java
@@ -61,9 +61,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**")
-                .denyAll()
-                .antMatchers("/").hasAnyRole(ROLE_USER, ROLE_ADMIN)
+                .antMatchers("/**").hasAnyRole(ROLE_USER, ROLE_ADMIN)
                 .antMatchers("/admin/**").hasRole(ROLE_ADMIN)
                 .and()
                 .formLogin()
